339868a1860bce1f6a7821988bdab84a07177aee
Ayush Srivastava <ayushs.1992@gmail.com>
2025-04-06 23:12:43 +0530

    Added Client app.

---
+++ client/pages/index.js
@@ -0,0 +1,15 @@
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  return <h1>{currentUser ? 'You are signed in' : 'You are not signed in'}</h1>;
+};
+
+LandingPage.getInitialProps = async (context) => {
+  console.log('Landing Page');
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default LandingPage;
