fd9e4b536b8d5aff8d85dcc8794bc53b456984d3
Ayush Srivastava <ayushs.1992@gmail.com>
2025-04-07 14:59:13 +0530

    Updated Auth service with common.

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { currentUser } from '../middleware/current-user';
+import { currentUser } from '@mitstickets/common';
 
 const router = express.Router();
 
